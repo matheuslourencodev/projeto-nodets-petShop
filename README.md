422ac9a0e32acefb5dfbcccca554055c8ffdbb23
Matheus Nathan Lourenço <matheusnathan31@gmail.com>
2021-12-23 11:29:59 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # projeto-nodets-petShop
+Projeto feito com Node + Typescript
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import  path  from "path/posix";
+import mainRoutes from './routes/index'
+
+dotenv.config()
+
+const server = express()
+const port = process.env.PORT || 3000;
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'))
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+// Rotas
+server.use(mainRoutes)
+
+server.use((req, res)=> {
+  res.send('Pagina nao encontrada');
+})
+
+
+
+server.listen(3000, ()=>{
+  console.log(`Rodando em http://localhost:${3000}`)
+});
